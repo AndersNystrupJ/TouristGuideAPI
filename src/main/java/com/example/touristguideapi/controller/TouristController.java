f2b73ee9db014dc83c7c85d91a040e4790687b04
AndersNystrupJ <anje0008@stud.kea.dk>
2024-09-05 15:33:25 +0200

    Commit - 05/09. Alle GET/POST

--- src/main/java/com/example/touristguideapi/controller/TouristController.java
+++ src/main/java/com/example/touristguideapi/controller/TouristController.java
@@ -1,6 +1,10 @@
 package com.example.touristguideapi.controller;
 
+import com.example.touristguideapi.model.TouristAttraction;
+import com.example.touristguideapi.repository.TouristRepository;
 import com.example.touristguideapi.service.TouristService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,5 +19,51 @@ public class TouristController {
         this.touristService = touristService;
     }
 
-    @GetMapping //Hm, det må jeg åbenbart ikke?
+    @GetMapping("")
+    public ResponseEntity<List<TouristAttraction>> getAllTouristAttractions(){
+        List<TouristAttraction> touristAttractions = touristService.getAllTouristAttractions();
+        return new ResponseEntity<>(touristAttractions, HttpStatus.OK);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<TouristAttraction> getAttractionByName(@PathVariable String name) {
+        TouristAttraction touristAttraction = touristService.findAttractionByName(name);
+        return new ResponseEntity<>(touristAttraction,HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<TouristAttraction> addAttraction(@RequestBody TouristAttraction touristAttraction){
+        TouristAttraction newTouristAttraction = touristService.addTouristAttraction(touristAttraction);
+        return new ResponseEntity<>(newTouristAttraction, HttpStatus.CREATED);
+    }
+    /*
+    @PostMapping("/change")
+    public ResponseEntity<TouristAttraction>changeAttraction(@RequestBody TouristAttraction touristAttraction, String updatedName, String updatedDescription){
+        TouristAttraction changedTouristAttraction = touristService.changeAttraction(touristAttraction,updatedName, updatedDescription );
+    return new ResponseEntity<>(newTouristAttraction, HttpStatus.CREATED);
+    }
+
+     */
+    @PostMapping("/update")
+    public ResponseEntity<TouristAttraction> updateAttraction(@RequestBody TouristAttraction touristAttraction){
+        TouristAttraction updateTouristAttraction = touristService.updateAttraction(touristAttraction);
+        if (updateTouristAttraction != null) {
+            return ResponseEntity.ok(updateTouristAttraction);
+        }else{
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+                }
+            }
+
+    @DeleteMapping("delete/name")
+    public ResponseEntity<String> deleteAttractionByName(@RequestBody TouristAttraction touristAttraction){
+    String attractionName = touristAttraction.getName();
+    boolean isDeleted = touristService.deleteTouristAttractionByName(attractionName);
+
+    if (isDeleted) {
+        return new ResponseEntity<>("Attraction deleted succesfully", HttpStatus.OK);
+    }else {
+        return new ResponseEntity<>("Attraction not found", HttpStatus.NOT_FOUND);
+    }
+
+    }
 }
--- src/main/java/com/example/touristguideapi/service/TouristService.java
+++ src/main/java/com/example/touristguideapi/service/TouristService.java
@@ -18,5 +18,29 @@ public class TouristService {
     public List<TouristAttraction> getAllTouristAttractions() {
         return touristRepository.getAllTouristAttractions();
     }
+    //GETMAPPING-ATTRACTION{NAME}
+    public TouristAttraction findAttractionByName(String name) {
+        return touristRepository.findAttractionByName(name);
+    }
+
+    //POSTMAPPING
+    public TouristAttraction addTouristAttraction(TouristAttraction touristAttraction){
+        return touristRepository.addAttraction(touristAttraction);
+    }
+
+    public TouristAttraction updateAttraction(TouristAttraction touristAttraction){
+       touristRepository.updateAttraction(touristAttraction);
+       return touristAttraction;
+    }
+
+    public boolean deleteTouristAttractionByName(String name) {
+        List<TouristAttraction> attractions = getAllTouristAttractions();
+
+        // Find the attraction by name and remove it
+        return attractions.removeIf(attraction -> attraction.getName().equalsIgnoreCase(name));
+    }
+
+
+
 
 }
